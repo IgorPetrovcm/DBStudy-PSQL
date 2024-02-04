a3c6d9edc869431f5736b5c5a7229b0846b79d47
Honor <igorpetrov42ru@gmail.com>
2024-02-04 22:43:17 +0300

    Дописал скрипт импорта данных

--- GeneralPractics/fromCollege/Semester4/pr1_2/implementation/main.sql
+++ GeneralPractics/fromCollege/Semester4/pr1_2/implementation/main.sql
@@ -34,13 +34,15 @@ CREATE TABLE positions (
 );
 
 CREATE TABLE appointments (
-    id INTEGER NOT NULL PRIMARY KEY,
+    id SERIAL NOT NULL PRIMARY KEY,
     appoint_date TIMESTAMP,
     dismissal_date TIMESTAMP,
     position_id INTEGER REFERENCES positions(id),
     department_id INTEGER REFERENCES departments(id),
     employee_id INTEGER REFERENCES employees(id)
 );
+
+SET datestyle = GERMAN,MDY;
 
 CREATE OR REPLACE PROCEDURE importData(path TEXT)
     LANGUAGE plpgsql
@@ -53,29 +55,18 @@ $$
             codeEmployee INTEGER, 
             nameEmployee VARCHAR(100),
             gender CHAR(1),
-            birthDate TIMESTAMP,
+            birthDate TIMESTAMPTZ,
             telephon VARCHAR(20),
             education VARCHAR(50),
             nameDepartment VARCHAR(50),
             codePosition INTEGER,
             namePosition VARCHAR(50),
             codeDepartment INTEGER,
-            dateAppoint TIMESTAMP,
-            dateDismissal TIMESTAMP
+            dateAppoint TIMESTAMPTZ,
+            dateDismissal TIMESTAMPTZ
         ) ;
 
-        EXECUTE format('COPY current_table (codeEmployee,
-        nameEmployee,
-        gender,
-        birthDate,
-        telephon,
-        education,
-        nameDepartment,
-        codePosition,
-        namePosition,
-        codeDepartment,
-        dateAppoint,
-        dateDismissal) FROM %L DELIMITER '','' CSV HEADER', path);
+        EXECUTE format('COPY current_table FROM %L DELIMITER '','' CSV HEADER', path);
 
         INSERT INTO education (name) 
         SELECT DISTINCT education FROM current_table;
@@ -86,10 +77,21 @@ $$
         INSERT INTO positions (id, name) 
         SELECT DISTINCT codePosition, namePosition FROM current_table;
 
-        INSERT INTO employees (id, last_name,first_name,sur_name) 
-        SELECT codeEmployee, SPLIT_PART(nameEmployee,' ',1), SPLIT_PART(nameEmployee,' ', 2), SPLIT_PART(nameEmployee,' ',3) 
-		FROM current_table;
+        INSERT INTO employees (id, last_name,first_name,sur_name,telephon,education_id) 
+        SELECT codeEmployee, SPLIT_PART(nameEmployee,' ',1), SPLIT_PART(nameEmployee,' ', 2), SPLIT_PART(nameEmployee,' ',3),
+                telephon, education.id  
+		FROM current_table
+        INNER JOIN education ON education.name = education;
+
+        INSERT INTO appointments (appoint_date, dismissal_date, position_id,department_id,employee_id)
+        SELECT DISTINCT dateAppoint, dateDismissal, positions.id, departments.id, employees.id
+        FROM current_table
+        INNER JOIN positions ON positions.name = namePosition
+        INNER JOIN departments ON departments.name = nameDepartment
+        INNER JOIN employees ON (employees.last_name || ' ' || employees.first_name || ' ' || employees.sur_name) = nameEmployee;
+
+        DROP TABLE current_table;
     END;
 $$;
 
-CALL importData('C:\Users\Honor\Desktop\Programming\DBStudy-PSQL\GeneralPractics\fromCollege\Semester4\pr1_2\csvСотрудникиИмпорт.csv');
+CALL importData('C:\Users\Honor\Desktop\Programming\DBStudy-PSQL\GeneralPractics\fromCollege\Semester4\pr1_2\csvImportEmployee.csv');
---
+++ GeneralPractics/fromCollege/Semester4/pr1_2/implementation/views-for-main-script.sql
@@ -0,0 +1,9 @@
+SELECT * FROM employees;
+
+SELECT * FROM departments; 
+
+SELECT * FROM positions; 
+
+SELECT * FROM education; 
+
+SELECT * FROM appointments; 
